cef107fac2ec39207402564052d8915795915325
Mahshid Ahmadian <51200485+mahshidahmadian@users.noreply.github.com>
2025-05-01 13:08:23 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,52 +5,51 @@
 */
 
 skel.init({
-	prefix: 'css/style',
-	resetCSS: true,
-	boxModel: 'border',
-	grid: {
-		gutters: 50
-	},
-	breakpoints: {
-		'mobile': {
-			range: '-480',
-			lockViewport: true,
-			containers: 'fluid',
-			grid: {
-				collapse: true,
-				gutters: 10
-			}
-		},
-		'desktop': {
-			range: '481-',
-			containers: 1200
-		},
-		'1000px': {
-			range: '481-1200',
-			containers: 960
-		}
-	}
+    prefix: 'css/style',
+    resetCSS: true,
+    boxModel: 'border',
+    grid: {
+        gutters: 50
+    },
+    breakpoints: {
+        'mobile': {
+            range: '-736',
+            lockViewport: false, // Allow zooming
+            containers: 'fluid',
+            grid: {
+                collapse: true,
+                gutters: 10
+            },
+            href: 'css/style-mobile.css' // Add mobile-specific CSS
+        },
+        '1000px': {
+            range: '737-1200', // Adjusted to start after mobile
+            containers: 960
+        },
+        'desktop': {
+            range: '1201-', // Start after 1000px breakpoint
+            containers: 1200
+        }
+    }
 }, {
-	panels: {
-		panels: {
-			navPanel: {
-				breakpoints: 'mobile',
-				position: 'left',
-				style: 'reveal',
-				size: '80%',
-				html: '<div data-action="navList" data-args="nav"></div>'
-			}
-		},
-		overlays: {
-			titleBar: {
-				breakpoints: 'mobile',
-				position: 'top-left',
-				height: 44,
-				width: '100%',
-				html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>'
-			}
-		}
-	}
-	
-	
+    panels: {
+        panels: {
+            navPanel: {
+                breakpoints: 'mobile',
+                position: 'left',
+                style: 'reveal',
+                size: '80%',
+                html: '<div data-action="navList" data-args="nav"></div>'
+            }
+        },
+        overlays: {
+            titleBar: {
+                breakpoints: 'mobile',
+                position: 'top-left',
+                height: 44,
+                width: '100%',
+                html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>'
+            }
+        }
+    }
 });
